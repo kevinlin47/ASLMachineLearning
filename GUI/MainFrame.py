aa2650265d3acbe275d5ad5e700a43a95a1f9df8
Kevin Lin <kevinlin47@hotmail.com>
2018-04-03 15:03:03 -0400

    test buttons

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -1,1 +1,15 @@
-print('hello world')
+import tkinter
+from tkinter import *
+
+top = tkinter.Tk()
+top.title("Program B.V-1")
+top.resizable(0, 0)
+top.geometry("500x500")
+
+upload_button = Button(top, text="one")
+cancel_button = Button(top, text="two")
+
+upload_button.pack()
+cancel_button.pack()
+
+top.mainloop()
