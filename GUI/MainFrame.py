c8b5907db3a73e84774e633b5f8f0a2b91c3ef2a
Kevin Lin <kevinlin@nbp-192-187.nbp.ruw.rutgers.edu>
2018-04-04 14:02:58 -0400

    Open Video Dialog

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -1,16 +1,27 @@
 import tkinter
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+
+
+# Function for uploading button
+def import_video():
+    file_name = askopenfilename(filetypes=(("MP4 Files", "*.mp4"), ("All files", "*.*")))
+    print(file_name)
+
 
 top = tkinter.Tk()  # Create root window
 top.title("Program B.V-1")  # Root window title
 top.resizable(0, 0)  # Do not allow user to resize the window
 top.geometry("500x500")  # Set window size to 500x500 pixels
 
-upload_button = Button(top, text="one")  # Button for video upload
-cancel_button = Button(top, text="two")  # Button for canceling request
+import_button = Button(top, text="Import Video", command=import_video)  # Button for video upload
+cancel_button = Button(top, text="Cancel")  # Button for canceling request
+
+import_button.config(height=1, width=10)
+cancel_button.config(height=1, width=10)
 
 # Testing tkinter widget layout manager
-upload_button.pack()
+import_button.pack()
 cancel_button.pack()
 
 top.mainloop()
