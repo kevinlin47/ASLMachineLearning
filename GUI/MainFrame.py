4e3f87e262a6fe63294b670f807fe1a7c6ea9a3f
Kevin Lin <kevinlin47@hotmail.com>
2018-04-04 12:50:56 -0400

    Comments

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -6,9 +6,10 @@ top.title("Program B.V-1")  # Root window title
 top.resizable(0, 0)  # Do not allow user to resize the window
 top.geometry("500x500")  # Set window size to 500x500 pixels
 
-upload_button = Button(top, text="one")
-cancel_button = Button(top, text="two")
+upload_button = Button(top, text="one")  # Button for video upload
+cancel_button = Button(top, text="two")  # Button for canceling request
 
+# Testing tkinter widget layout manager
 upload_button.pack()
 cancel_button.pack()
 
