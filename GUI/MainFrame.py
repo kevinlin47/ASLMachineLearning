432391dbc91688620f9c107fe4867aee6a93ca66
Kevin Lin <kevinlin@nbp-192-187.nbp.ruw.rutgers.edu>
2018-04-04 12:51:36 -0400

    Added Comments

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -1,10 +1,10 @@
 import tkinter
 from tkinter import *
 
-top = tkinter.Tk()
-top.title("Program B.V-1")
-top.resizable(0, 0)
-top.geometry("500x500")
+top = tkinter.Tk()  # Create root window
+top.title("Program B.V-1")  # Root window title
+top.resizable(0, 0)  # Do not allow user to resize the window
+top.geometry("500x500")  # Set window size to 500x500 pixels
 
 upload_button = Button(top, text="one")
 cancel_button = Button(top, text="two")
