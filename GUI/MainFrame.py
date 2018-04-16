aca743e57ee228f3dba230bae9b1c6dc9a7c1f07
Kevin Lin <kevinlin@nbp-244-151.nbp.ruw.rutgers.edu>
2018-04-16 16:46:23 -0400

    asdsad

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -5,14 +5,14 @@ from tkinter.filedialog import askopenfilename
 
 def import_video():  # Function for uploading button
     file_name = askopenfilename(filetypes=(("MP4 Files", "*.mp4"), ("All files", "*.*")))
-    print(file_name)
+    video_label.config(text=file_name)
 
-    video_label = Label(top, anchor=CENTER, text=file_name)
-    video_label.pack()
+    print(file_name)
 
 
 def cancel_video():  # Function for cancel button
     # Clear window
+    video_label.config(text="")
     print()
 
 
@@ -22,7 +22,9 @@ top.resizable(0, 0)  # Do not allow user to resize the window
 top.geometry("500x500")  # Set window size to 500x500 pixels
 
 import_button = Button(top, text="Import Video", command=import_video)  # Button for video upload
-cancel_button = Button(top, text="Cancel")  # Button for canceling request
+cancel_button = Button(top, text="Cancel", command=cancel_video)  # Button for canceling request
+exit_button= Button(top, text="Exit", command=top.destroy).pack()
+video_label = Label(top, anchor=CENTER, text="");
 
 import_button.config(height=1, width=10)
 cancel_button.config(height=1, width=10)
@@ -30,5 +32,6 @@ cancel_button.config(height=1, width=10)
 # Testing tkinter widget layout manager
 import_button.pack()
 cancel_button.pack()
+video_label.pack()
 
 top.mainloop()
