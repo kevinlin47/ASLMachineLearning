bc2c9fc95ed39a8dcdf0cc06ae8d031ed38935ba
Kevin Lin <kevinlin47@hotmail.com>
2018-04-16 22:14:48 -0400

    comments

--- GUI/MainFrame.py
+++ GUI/MainFrame.py
@@ -4,16 +4,17 @@ from tkinter.filedialog import askopenfilename
 
 
 def import_video():  # Function for uploading button
+    # set file_name to string of video directory
     file_name = askopenfilename(filetypes=(("MP4 Files", "*.mp4"), ("All files", "*.*")))
-    video_label.config(text=file_name)
 
-    print(file_name)
+    video_label.config(text=file_name)  # set label to show video directory
+
+    print(file_name)  # print test
 
 
 def cancel_video():  # Function for cancel button
     # Clear window
     video_label.config(text="")
-    print()
 
 
 top = tkinter.Tk()  # Create root window
@@ -23,12 +24,12 @@ top.geometry("500x500")  # Set window size to 500x500 pixels
 
 import_button = Button(top, text="Import Video", command=import_video)  # Button for video upload
 cancel_button = Button(top, text="Cancel", command=cancel_video)  # Button for canceling request
-exit_button = Button(top, text="Exit", command=top.destroy)
-video_label = Label(top, anchor=CENTER, text="");
+exit_button = Button(top, text="Exit", command=top.destroy)  # Button for closing the application
+video_label = Label(top, anchor=CENTER, text="")  # text label
 
-import_button.config(height=1, width=10)
-cancel_button.config(height=1, width=10)
-exit_button.config(height=1, width=10)
+import_button.config(height=1, width=10)  # resize the button
+cancel_button.config(height=1, width=10)  # resize the button
+exit_button.config(height=1, width=10)    # resize the button
 
 # Testing tkinter widget layout manager
 import_button.pack()
